8ca17e012a6685b7eab64dce7d8381d3ba6d0a6e
Bogdan POPA <bpopa@pentalog.fr>
2017-11-15 13:16:00 +0200

    Made validation methods private

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,12 +1,15 @@
 class Rank < ActiveRecord::Base
+
+  validate :score_from_value
+  validate :cannot_overlap_scores
+  validates_numericality_of :score_from, :score_to, with: /\A[0-9]+\.[0-9]{1}\z/
   validates :name, presence: true
 
   scope :score_overlap, ->(score_from) do
     where "((score_from <= ?) and (score_to >= ?))", score_from, score_from
   end
 
-  validate :score_from_value
-  validate :cannot_overlap_scores
+  private
 
   def score_from_value
     unless lower_than_score_to?
@@ -19,8 +22,7 @@ class Rank < ActiveRecord::Base
   end
 
   def cannot_overlap_scores
-    score_overlaps = Rank.score_overlap(score_from)
-    score_overlap_err unless score_overlaps.empty?
+    score_overlap_err unless Rank.score_overlap(score_from).empty?
   end
 
   def score_overlap_err
