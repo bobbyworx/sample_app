39479fbb7f27fbe2e39c40fecf1b34a50cd76041
Bogdan POPA <bpopa@pentalog.fr>
2017-11-15 13:13:36 +0200

    Few specs for ranks score validations

--- spec/factories/ranks.rb
+++ spec/factories/ranks.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :ranks do
+  factory :rank do
     score_from { Faker::Number.decimal(2) }
     score_to { Faker::Number.decimal(2) }
     name 'test rank'
--- spec/models/rank_spec.rb
+++ spec/models/rank_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 RSpec.describe Rank, type: :model do
 
   describe 'rank' do
+    subject { build(:rank) }
     it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:score_from) }
+    it { should validate_numericality_of(:score_to) }
   end
 
   describe 'ranks attrs' do
     it 'checks if score_from is less than score_to' do
-      rank = Rank.new(score_from: 2.4, score_to: 5.2, name: 'example rank' )
+      rank = Rank.create(score_from: 2.4, score_to: 5.2, name: 'example rank' )
       expect(rank.score_to).to be > rank.score_from
+    end
+
+    it 'checks for overlaping rank scores' do
+      rank_one = Rank.create(score_from: 2.4, score_to: 5.2, name: 'example rank' )
+      rank_two = Rank.create(score_from: 4.0, score_to: 8, name: 'example rank two' )
+
+      expect(Rank.score_overlap(rank_two.score_from).size).to eq 1
+      expect(rank_two.score_overlap_err.first).to include('overlaps with')
     end
   end
 
